2207cd0d672a3ff33172fe773112875414e873bd
captaiiinsolo <solo.d.santos@gmail.com>
2023-02-07 19:59:01 -0800

    create connection to database

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+// Imports Sequelize library
+const Sequelize = require('sequelize');
+
+// Imports dotenv library
+require('dotenv').config();
+
+// Creeates new instance of Sequelize and passes envirnmental variables
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
+
+// exports sequelize to be used in other files
+module.exports = sequelize;
