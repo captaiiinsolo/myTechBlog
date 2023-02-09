e8d1b338ea6a147be93a462065fb35c6298a7e59
captaiiinsolo <solo.d.santos@gmail.com>
2023-02-08 17:27:15 -0800

    add post_date to post model

--- models/Post.js
+++ models/Post.js
@@ -20,6 +20,12 @@ Post.init(
       },
     },
 
+    post_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+
     contents: {
       type: DataTypes.TEXT,
       allowNull: false,
