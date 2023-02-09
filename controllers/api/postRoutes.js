c2213fb1ffbe92fe59b2f61ad57a00ee4aede1a5
captaiiinsolo <solo.d.santos@gmail.com>
2023-02-09 01:05:42 -0800

    update /api comment and post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
-const withAuth = require('../../utils');
+const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -14,6 +14,30 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatedPost = await Post.update(
+            {
+                title: req.body.title,
+                contents: req.body.contents,
+            },
+
+            {
+                where: {
+                    id: req.params.id,
+                },
+            },
+        );
+
+        res.status(200).json({ message: 'Post successfully updated!', updatedPost });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+    
 
 
 router.delete('/:id', withAuth, async (req, res) => {
